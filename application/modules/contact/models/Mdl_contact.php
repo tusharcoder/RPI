0137721a98fab4d75c911e8ce9193851ce229da4
scaledesk_tushar <tushar@scaledesk.com>
2015-11-16 19:07:31 +0530

    start adding contact page module

--- application/modules/contact/models/Mdl_contact.php
+++ application/modules/contact/models/Mdl_contact.php
@@ -6,6 +6,25 @@
  * Time: 6:58 PM
  */
 
-class Mdl_contact {
+class Mdl_contact extends CI_Model{
+    /**
+    * Model data
+    */
+    private $id;
+    private $address;
+    private $person;
+    private $mobile;
+    private $email_id;
+
+    /**
+     * constants
+     */
+    const TABLE="contact";
+    const ID="contact_id";
+    const PERSON="contact_person";
+    const ADDRESS="contact_address";
+    const MOBILE="contact_mobile";
+    const EMAIL_ID="contact_email_id";
+
 
 }
